d95ffb0bb99c043db68349e83048b0e8f4e37159
rafanoel <rafanoel@gmail.com>
2014-07-28 23:50:50 -0500

    Implementing Tabs
    Test 1

---
+++ SFM/src/com/apdimo/sfm/TabBar.java
@@ -0,0 +1,15 @@
+package com.apdimo.sfm;
+
+import android.app.Activity;
+import android.app.TabActivity;
+import android.widget.TabHost.OnTabChangeListener;
+
+public class TabBar extends TabActivity implements OnTabChangeListener {
+
+	@Override
+	public void onTabChanged(String tabId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
